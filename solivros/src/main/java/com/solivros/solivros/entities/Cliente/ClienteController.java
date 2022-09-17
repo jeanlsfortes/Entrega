2343d352f1545cfb8f316415cb54e2b4f35fb1e6
jeanls-solano <jeanlucaseh10@gmail.com>
2022-09-16 22:02:28 -0400

    insert inicial dos clientes no BD

--- solivros/src/main/java/com/solivros/solivros/entities/Cliente/ClienteController.java
+++ solivros/src/main/java/com/solivros/solivros/entities/Cliente/ClienteController.java
@@ -1,13 +1,23 @@
 package com.solivros.solivros.entities.Cliente;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
     
     @GetMapping("/clientes")
-    public String testando(){
-        return "tudo mec manito";
+    public List<Cliente> listarClientes(){
+        return clienteRepository.findAll();
     }
 }
---
+++ solivros/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Carlos André Pereira','carlinhos@hotmail.com','88 99971-3465');
+insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Jean Lucass Salves','jeanluc@hotmail.com','67 99789-3265');
+insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Victor Pereira Andrade','victor@gmail.com','62 7890-3465');
+insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Ana Maria Ciqueira','anaMaria@hotmail.com','87 1234-3465');
+insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Pedro Paulo José','pedro@gmail.com','99 99911-1465');
