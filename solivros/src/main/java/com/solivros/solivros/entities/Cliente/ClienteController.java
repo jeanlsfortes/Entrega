798e1aff2b8c56cb6769a386576d9a3bd023a243
jeanls-solano <jeanlucaseh10@gmail.com>
2022-09-17 10:51:56 -0400

    inplementando crup básica da entidade cliente

--- solivros/src/main/java/com/solivros/solivros/entities/Cliente/ClienteController.java
+++ solivros/src/main/java/com/solivros/solivros/entities/Cliente/ClienteController.java
@@ -1,14 +1,25 @@
 package com.solivros.solivros.entities.Cliente;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import lombok.AllArgsConstructor;
 
+@RequestMapping("/cliente")
 @AllArgsConstructor
 @RestController
 public class ClienteController {
@@ -16,8 +27,44 @@ public class ClienteController {
     @Autowired
     private ClienteRepository clienteRepository;
     
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listarClientes(){
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long id){
+        return clienteRepository.findById(id)
+            .map(cliente -> ResponseEntity.ok(cliente))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping
+    public ResponseEntity<Cliente>  editar(@PathVariable Long id, @RequestBody Cliente cliente){
+        if(clienteRepository.existsById(id)){
+            cliente.setId(id);
+            cliente = clienteRepository.save(cliente);
+
+            return ResponseEntity.ok(cliente);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping 
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        if(clienteRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        clienteRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- solivros/src/main/resources/data.sql
+++ solivros/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Carlos André Pereira','carlinhos@hotmail.com','88 99971-3465');
+insert into CLIENTE(NOME, EMAIL, TELEFONE) values('Carlos André Pereira','carlinhos@hotmail.com','88 99971-3465');
 insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Jean Lucass Salves','jeanluc@hotmail.com','67 99789-3265');
 insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Victor Pereira Andrade','victor@gmail.com','62 7890-3465');
 insert into CLIENTE( NOME, EMAIL, TELEFONE) values('Ana Maria Ciqueira','anaMaria@hotmail.com','87 1234-3465');
