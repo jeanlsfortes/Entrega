ece5d53e08012c77295dd57aa2c291830eb0c1f2
jeanls-solano <jeanlucaseh10@gmail.com>
2022-09-17 11:07:28 -0400

    inplementando a validacao com o bean validation

--- solivros/src/main/java/com/solivros/solivros/entities/Cliente/Cliente.java
+++ solivros/src/main/java/com/solivros/solivros/entities/Cliente/Cliente.java
@@ -1,10 +1,15 @@
 package com.solivros.solivros.entities.Cliente;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -17,10 +22,16 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @NotNull
+    @Size(max = 50)
     private String nome;
     
+    @Email
     private String email;
 
+    @Column(name = "telefone")
+    @Size(max = 20, min = 9)
     private String telefone;
 
 }
